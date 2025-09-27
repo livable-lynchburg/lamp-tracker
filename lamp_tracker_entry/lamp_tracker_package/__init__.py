e81478078efdd2d3b696a70f7fbb4d0c9007db52
Nathan Wyand <me@nathanwyand.com>
2025-09-27 17:19:19 -0400

    Try configuring app before using it

--- lamp_tracker_entry/lamp_tracker_package/__init__.py
+++ lamp_tracker_entry/lamp_tracker_package/__init__.py
@@ -3,6 +3,10 @@ from flask import Flask
 # TODO LOOK INTO INSTANCE FOLDER CONCEPT
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 
+
+# importing app configuration from Python object
+app.config.from_object("config.Config")
+
 #THIS IMPORT AND INSTANTIATION MUST COME SECOND
 from flask_login import LoginManager
 login_manager = LoginManager(app)
@@ -10,8 +14,5 @@ login_manager.login_view = "log_in"
 login_manager.login_message_category = "info"
 
 from lamp_tracker_package import views
-
-# importing app configuration from Python object
-app.config.from_object("config.Config")
 
 # TODO move db instantiation here
