e1350e49af305b0181f66c31c950b3d3ff9666f8
Nathan Wyand <me@nathanwyand.com>
2025-10-23 08:52:45 -0400

    update db model

--- lamp_tracker_entry/lamp_tracker_package/db.py
+++ lamp_tracker_entry/lamp_tracker_package/db.py
@@ -2,7 +2,7 @@ from lamp_tracker_package import app, login_manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from geoalchemy2.types import Geography
-from sqlalchemy import inspect, event, text, Integer, BigInteger, String, Unicode
+from sqlalchemy import inspect, event, text, Integer, bigint, String, unicode
 from sqlalchemy.sql.expression import and_
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
@@ -38,27 +38,27 @@ class User(db.Model, UserMixin):
     role: Mapped[unicode] = mapped_column(nullable=False) #200chars
 
 class Outage(db.Model):
-    lamp_id = db.Column(db.BigInteger, db.ForeignKey("lamp.id"), primary_key=True)
-    found_dtm = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, primary_key=True)
-    report_dtm = db.Column(db.DateTime, nullable=True);
-    checked_dtm = db.Column(db.DateTime, nullable=True);
-    fixed_dtm = db.Column(db.DateTime, nullable=True);
-    most_recent_contact_dtm = db.Column(db.DateTime, nullable=True);
-    report_dtm = db.Column(db.DateTime, nullable=True);
-    work_order_id = db.Column(db.Unicode);
+    lamp_id: Mapped[bigint] = mapped_column(ForeignKey("lamp.id"), primary_key=True)
+    found_dtm: Mapped[datetime] = mapped_column(nullable=False, default=datetime.utcnow, primary_key=True)
+    report_dtm: Mapped[datetime] = mapped_column(nullable=True)
+    checked_dtm: Mapped[datetime] = mapped_column(nullable=True)
+    fixed_dtm: Mapped[datetime] = mapped_column(nullable=True)
+    most_recent_contact_dtm: Mapped[datetime] = mapped_column(nullable=True)
+    report_dtm: Mapped[datetime] = mapped_column(nullable=True)
+    work_order_id: Mapped[unicode] = mapped_column()
 
 class Lamp(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
-    store_id = db.Column(db.BigInteger, db.ForeignKey("store.id"), primary_key=True)
-    change_dtm = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, primary_key=True)
-    location = db.Column(Geography("POINT"))
-    #   Chose BigInteger type because it is used for IDs in OSM's internal schema
-    osm_id = db.Column(db.BigInteger)
-    verizon_id = db.Column(db.Unicode);
-    appco_id = db.Column(db.Unicode);
-    lynchburg_id = db.Column(db.Unicode);
-    addr_housenumber = db.Column(db.Unicode)
-    addr_street = db.Column(db.Unicode)
-    addr_city = db.Column(db.Unicode)
-    addr_state = db.Column(db.Unicode)
-    addr_postcode = db.Column(db.Unicode)
+    id: Mapped[bigint] = mapped_column(primary_key=True)
+    store_id: Mapped[bigint] = mapped_column(ForeignKey("store.id"), primary_key=True)
+    change_dtm: Mapped[datetime] = mapped_column(nullable=False, default=datetime.utcnow, primary_key=True)
+    location: Mapped['Geography'] = mapped_column(Geography("POINT"))
+    #   Chose bigint type because it is used for IDs in OSM's internal schema
+    osm_id: Mapped[bigint] = mapped_column()
+    verizon_id: Mapped[unicode] = mapped_column()
+    apco_id: Mapped[unicode] = mapped_column()
+    lynchburg_id: Mapped[unicode] = mapped_column()
+    addr_housenumber: Mapped[unicode] = mapped_column()
+    addr_street: Mapped[unicode] = mapped_column()
+    addr_city: Mapped[unicode] = mapped_column()
+    addr_state: Mapped[unicode] = mapped_column()
+    addr_postcode: Mapped[unicode] = mapped_column()
